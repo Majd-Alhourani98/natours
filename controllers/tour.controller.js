cc00e0cb55a3303d4f34aa5e3ab7424d0537502d
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-11-15 16:28:53 +0300

    feat(api): add pagination logic with page boundary validation in getAllTours

--- controllers/tour.controller.js
+++ controllers/tour.controller.js
@@ -34,9 +34,7 @@ const getAllTours = async (req, res) => {
 
     // 3) Sorting
     if (req.query.sort) {
-      console.log(req.query);
       const sortBy = req.query.sort.split(',').join(' ');
-      console.log(sortBy);
       query = query.sort(sortBy);
     } else {
       // by default sort by newest
@@ -51,8 +49,18 @@ const getAllTours = async (req, res) => {
       query = query.select('-__v -createdAt -updatedAt');
     }
 
-    // Execute the Query
+    // 5) Pagination
+    const page = Number(req.query.page) || 1;
+    const limit = Number(req.query.limit) || 100;
+    const skip = (page - 1) * limit;
 
+    query = query.skip(skip).limit(limit);
+
+    const totalDocs = await Tour.countDocuments();
+    if (skip >= totalDocs) {
+      throw new Error('This page does not exist');
+    }
+    // Execute the Query
     const tours = await query;
 
     res.status(200).json({
@@ -65,7 +73,7 @@ const getAllTours = async (req, res) => {
   } catch (error) {
     res.status(404).json({
       status: 'fail',
-      message: error,
+      message: error.message,
     });
   }
 };
