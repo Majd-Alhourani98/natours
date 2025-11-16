7e0f47c6b2e7cc822e2cfad6d02cf13f35643b68
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-11-16 19:40:44 +0300

    add data validation with built in mongoose's validator

--- models/tour.model.js
+++ models/tour.model.js
@@ -8,8 +8,12 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
     },
+
     slug: String,
+
     duration: {
       type: Number,
       required: [true, 'A tour must have a duration'],
@@ -23,11 +27,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
 
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
 
     ratingsQuantity: {
