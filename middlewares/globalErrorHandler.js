fad5e35995471bcb83977658dc795ba91e4e40be
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-11-17 21:23:56 +0300

    feat(errorHandling): implement dev/prod error response formatting

--- middlewares/globalErrorHandler.js
+++ middlewares/globalErrorHandler.js
@@ -1,10 +1,41 @@
+const env = require('./../config/env.config');
+
+const isDevelopment = env.FLAGS.isDevelopment;
+const isProduction = env.FLAGS.isProduction;
+
+const sendErrorDev = (err, res) => {
+  res.status(err.statusCode).json({
+    status: err.status,
+    error: err,
+    message: err.message,
+    stack: err.stack,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  // Operational Error, trusted error: Send message to client
+  if (err.isOperational) {
+    res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    // Programming or other unknown error: don't leak error detailes
+    // 1) log error
+    console.error('ERROR 💥', err);
+    // 2) Send generic message
+    res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong',
+    });
+  }
+};
+
 module.exports = (err, req, res, next) => {
   console.log(err.message);
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'err';
 
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (isDevelopment) sendErrorDev(err, res);
+  else if (isProduction) sendErrorProd(err, res);
 };
