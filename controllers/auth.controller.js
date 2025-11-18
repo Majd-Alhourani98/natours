251f08756cbd3a09f5edc056e17f0a073b1ce8d8
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2025-11-19 00:34:27 +0300

    send a token to the client

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,14 +1,31 @@
+const jwt = require('jsonwebtoken');
+
 const User = require('../models/user.model');
 const sendSuccess = require('../utils/responseHandler');
 const HTTP_STATUS = require('./../constants/httpStatus');
 const catchAsync = require('./../utils/catchAsync');
+const env = require('./../config/env.config');
 
 const signup = catchAsync(async (req, res, next) => {
-  const user = await User.create(req.body);
+  const { name, email, password, passwordConfirm } = req.body;
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+  });
+
+  const token = jwt.sign({ id: user._id }, env.JWT.SECRET, {
+    expiresIn: env.JWT.EXPIRES_IN,
+  });
+
+  user.password = undefined;
 
   sendSuccess(res, {
     statusCode: HTTP_STATUS.CREATED,
     data: { user },
+    token,
   });
 });
 
--- utils/responseHandler.js
+++ utils/responseHandler.js
@@ -1,12 +1,20 @@
 const sendSuccess = (
   res,
-  { statusCode = 200, message = 'Success', data = null, meta = null, results = null } = {}
+  {
+    statusCode = 200,
+    message = 'Success',
+    data = null,
+    meta = null,
+    results = null,
+    token = null,
+  } = {}
 ) => {
   const response = { status: 'success', message };
 
   if (results !== null) response.results = results;
   if (data !== null) response.data = data;
   if (meta !== null) response.meta = meta;
+  if (token !== null) response.token = token;
 
   return res.status(statusCode).json(response);
 };
