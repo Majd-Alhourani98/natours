e0784d90d77bd39ceb91cff5b9ccb79b0ab39cb7
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2025-12-21 10:23:51 -0800

    feat: add Tour mongoose schema and model

---
+++ models/tour.mode.js
@@ -0,0 +1,79 @@
+const mongoose = require('mongoose');
+
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'Name is required'],
+      unique: true,
+      trim: true,
+    },
+
+    duration: {
+      type: Number,
+      required: [true, 'Duration is required'],
+    },
+
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'Group size is required'],
+    },
+
+    difficulty: {
+      type: String,
+      required: [true, 'Difficulty is required'],
+    },
+
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+    },
+
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+
+    price: {
+      type: Number,
+      required: [true, 'Price is required'],
+    },
+
+    priceDiscount: {
+      type: Number,
+    },
+
+    summary: {
+      type: String,
+      trim: true,
+      required: [true, 'Summary is required'],
+    },
+
+    description: {
+      type: String,
+      trim: true,
+    },
+
+    imageCover: {
+      type: String,
+      required: [true, 'Cover image is required'],
+    },
+
+    images: {
+      type: [String],
+      default: [],
+    },
+
+    startDates: {
+      type: [Date],
+      default: [],
+    },
+  },
+  {
+    timestamps: true,
+  }
+);
+
+const Tour = mongoose.model('Tour', tourSchema);
+
+module.exports = Tour;
