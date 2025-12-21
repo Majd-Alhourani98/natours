580659f611025933d64f0556629279f44cc85cc0
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2025-12-21 08:52:22 -0800

    feat(tours): add list tours endpoint

--- app.js
+++ app.js
@@ -9,13 +9,23 @@ app.get('/health', (req, res) => {
   });
 });
 
+app.get('/api/v1/tours', (req, res) => {
+  res.status(200).json({
+    success: true,
+    status: 'success',
+    message: 'Tours retrieved successfully',
+    results: '<number_of_tours>',
+    data: {
+      tours: '<list_of_tours>',
+    },
+  });
+});
+
 const PORT = 3000;
 app.listen(PORT, () => {
-  console.log('');
-  console.log('━'.repeat(20), `🔥 SERVER`, '━'.repeat(20));
+  console.log(`\n${'━'.repeat(20)} 🔥 SERVER ${'━'.repeat(20)}`);
   console.log(`🟢 STATUS      → Running`);
   console.log(`🔗 LINK        → http://localhost:${PORT}`.padStart(25));
   console.log(`🌍 ENVIRONMENT → ${app.get('env')}`);
-  console.log(`⏰ STARTED AT  → ${new Date().toLocaleTimeString()}`);
-  console.log('');
+  console.log(`⏰ STARTED AT  → ${new Date().toLocaleTimeString()}\n`);
 });
