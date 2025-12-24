6c8a9a83a5c4a4bfc4692e70bbe11b3722241877
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2025-12-24 23:11:20 +0200

    feat: integrate dotenv and cross-env for environment management

--- server.js
+++ server.js
@@ -1,13 +1,16 @@
+const dotenv = require('dotenv');
+dotenv.config();
+
 const app = require('./app');
 const connectDB = require('./config/database');
 
 connectDB();
 
-const PORT = 3000;
+const PORT = process.env.PORT || 5000;
 app.listen(PORT, () => {
   console.log(`\n${'━'.repeat(20)} 🔥 SERVER ${'━'.repeat(20)}`);
   console.log(`🟢 STATUS      → Running`);
   console.log(`🔗 LINK        → http://localhost:${PORT}`.padStart(25));
-  console.log(`🌍 ENVIRONMENT → ${app.get('env')}`);
+  console.log(`🌍 ENVIRONMENT → ${process.env.NODE_ENV}`);
   console.log(`⏰ STARTED AT  → ${new Date().toLocaleTimeString()}\n`);
 });
