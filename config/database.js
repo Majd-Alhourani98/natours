c59d8837595376db4bf0cd125eb78c2d2cd1231b
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2025-12-25 16:02:35 +0200

    feat(utils): add buildDatabaseURI helper

--- config/database.js
+++ config/database.js
@@ -1,12 +1,21 @@
 const mongoose = require('mongoose');
+const buildDatabaseURI = require('../utils/buildDatabaseURI');
 
-const DB_URI = 'mongodb://localhost:27017/natours';
+// const DB_URI = 'mongodb://localhost:27017/natours';
+
+const { DATABASE_URL, DATABASE_USERNAME, DATABASE_PASSWORD, DATABASE_NAME } = process.env;
+const DB_URI = buildDatabaseURI(
+  DATABASE_USERNAME,
+  DATABASE_PASSWORD,
+  DATABASE_USERNAME,
+  DATABASE_URL
+);
 
 const connectDB = async () => {
   try {
     await mongoose.connect(DB_URI);
 
-    console.log(`\n${'━'.repeat(20)} ⚡ DATABASE ${'━'.repeat(20)}`);
+    console.log(`${'━'.repeat(20)} ⚡ DATABASE ${'━'.repeat(20)}`);
     console.log(`🟢 STATUS   → Connected`);
     console.log(`📦 DATABASE → ${mongoose.connection.name}`);
     console.log(`🌐 HOST     → ${mongoose.connection.host}`);
