482a6070d980909c49dfc7ef579bd3619ef763e3
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2025-12-25 16:59:12 +0200

    feat(users): create User schema with validation for name, email, and password

---
+++ models/user.model.js
@@ -0,0 +1,36 @@
+const mongoose = require('mongoose');
+
+const userSchema = new mongoose(
+  {
+    name: {
+      type: String,
+      required: [true, 'Please tell us your name'],
+      trim: true,
+      minlength: [3, 'Name must be at least 3 characters'],
+      maxlength: [50, 'Name must be less than 50 characters'],
+    },
+
+    email: {
+      type: String,
+      required: [true, 'Please provide your email'],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [/^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/, 'Please provide a valid email'],
+    },
+
+    password: {
+      type: String,
+      required: [true, 'Please provide a password'],
+      minlength: [8, 'Password must be at least 8 characters'],
+    },
+
+    passwordConfirm: {
+      type: String,
+      required: [true, 'Please confirm your password'],
+    },
+  },
+  {
+    timestamp: true,
+  }
+);
