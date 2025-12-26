12c42257b1830fcc49a71819819f097e69acc9da
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2025-12-26 12:31:45 +0200

    feat(error-handling): add environment-based error responses for dev and prod

--- errors/handlers/errorHandler.js
+++ errors/handlers/errorHandler.js
@@ -1,11 +1,36 @@
+const sendErrorDev = (err, res) => {
+  return res.status(err.statusCode).json({
+    status: err.status,
+    message: err.message,
+    stack: err.stack,
+    error: err,
+  });
+};
+
+const sendErrorProd = (err, res) => {
+  if (err.isOperational) {
+    return res.status(err.statusCode).json({
+      status: err.status,
+      message: err.message,
+    });
+  } else {
+    console.error('ERROR 💥', err);
+    return res.status(500).json({
+      status: 'error',
+      message: 'Something went very wrong, Please try again later',
+    });
+  }
+};
+
 const errorHandler = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
   err.status = err.status || 'error';
 
-  return res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
+  if (process.env.NODE_ENV === 'development') {
+    sendErrorDev(err, res);
+  } else if (process.env.NODE_ENV === 'production') {
+    sendErrorProd(err, res);
+  }
 };
 
 module.exports = errorHandler;
