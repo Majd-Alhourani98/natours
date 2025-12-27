2e403ab55d619d1e033bed0e956f20fe836a889e
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2025-12-27 23:27:02 +0200

    feat(process): handle unhandled promise rejections

--- server.js
+++ server.js
@@ -7,10 +7,18 @@ const connectDB = require('./config/database');
 connectDB();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`\n${'━'.repeat(20)} 🔥 SERVER ${'━'.repeat(20)}`);
   console.log(`🟢 STATUS      → Running`);
   console.log(`🔗 LINK        → http://localhost:${PORT}`.padStart(25));
   console.log(`🌍 ENVIRONMENT → ${process.env.NODE_ENV}`);
   console.log(`⏰ STARTED AT  → ${new Date().toLocaleTimeString()}\n`);
+});
+
+process.on('unhandledRejection', (reason, promise) => {
+  console.error('❌ Unhandled Rejection at:', promise);
+  console.error('Reason:', reason instanceof Error ? reason.stack : reason);
+  server.close(() => {
+    process.exit(1);
+  });
 });
