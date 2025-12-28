9822c9275a2aad4583d252fe97f6bf8d3eba3ac3
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2025-12-28 19:42:47 +0200

    Refactor: Implement AppError subclasses using constants

--- errors/classes/appError.js
+++ errors/classes/appError.js
@@ -1,13 +1,121 @@
+const httpStatus = require('../../constants/httpStatus');
+const responseStatus = require('../../constants/responseStatus');
+const errorTypes = require('../../constants/errorTypes');
+const httpMessages = require('../../constants/httpMessages');
+
 class AppError extends Error {
-  constructor(message, statusCode = 500, isOperational = true) {
+  constructor(
+    message,
+    statusCode = httpStatus.INTERNAL_SERVER_ERROR,
+    code = errorTypes.INTERNAL_SERVER_ERROR,
+    isOperational = true
+  ) {
     super(message);
 
     this.statusCode = statusCode;
-    this.status = String(statusCode).startsWith('4') ? 'fail' : 'error';
+    this.status = `${statusCode}`.startsWith('4')
+      ? responseStatus.FAIL
+      : responseStatus.ERROR;
+    this.code = code;
     this.isOperational = isOperational;
 
     Error.captureStackTrace(this, this.constructor);
   }
 }
 
-module.exports = AppError;
+class BadRequestError extends AppError {
+  constructor(message = httpMessages.BAD_REQUEST) {
+    super(message, httpStatus.BAD_REQUEST, errorTypes.BAD_REQUEST);
+  }
+}
+
+class AuthenticationError extends AppError {
+  constructor(message = httpMessages.UNAUTHORIZED) {
+    super(message, httpStatus.UNAUTHORIZED, errorTypes.AUTHENTICATION_FAILED);
+  }
+}
+
+class ForbiddenError extends AppError {
+  constructor(message = httpMessages.FORBIDDEN) {
+    super(message, httpStatus.FORBIDDEN, errorTypes.FORBIDDEN);
+  }
+}
+
+class NotFoundError extends AppError {
+  constructor(message = httpMessages.NOT_FOUND) {
+    super(message, httpStatus.NOT_FOUND, errorTypes.NOT_FOUND);
+  }
+}
+
+class ConflictError extends AppError {
+  constructor(message = httpMessages.CONFLICT) {
+    super(message, httpStatus.CONFLICT, errorTypes.CONFLICT);
+  }
+}
+
+class ValidationError extends AppError {
+  constructor(message = httpMessages.UNPROCESSABLE_ENTITY) {
+    super(message, httpStatus.UNPROCESSABLE_ENTITY, errorTypes.VALIDATION_ERROR);
+  }
+}
+
+class TooManyRequestsError extends AppError {
+  constructor(message = httpMessages.TOO_MANY_REQUESTS) {
+    super(message, httpStatus.TOO_MANY_REQUESTS, errorTypes.TOO_MANY_REQUESTS);
+  }
+}
+
+class PayloadTooLargeError extends AppError {
+  constructor(message = httpMessages.PAYLOAD_TOO_LARGE) {
+    super(message, httpStatus.PAYLOAD_TOO_LARGE, errorTypes.PAYLOAD_TOO_LARGE);
+  }
+}
+
+class RequestTimeoutError extends AppError {
+  constructor(message = httpMessages.REQUEST_TIMEOUT) {
+    super(message, httpStatus.REQUEST_TIMEOUT, errorTypes.REQUEST_TIMEOUT);
+  }
+}
+
+// 5xx Server Errors
+class InternalServerError extends AppError {
+  constructor(message = httpMessages.INTERNAL_SERVER_ERROR) {
+    super(
+      message,
+      httpStatus.INTERNAL_SERVER_ERROR,
+      errorTypes.INTERNAL_SERVER_ERROR,
+      false // Internal errors might not be operational, but usually we treat handled ones as such? 
+      // Actually, AppError defaults isOperational to true. 
+      // InternalServerError usually implies something went wrong that wasn't user input. 
+      // However, if we are throwing it explicitly, it IS 'operational' in the sense that we caught it. 
+      // But standard practice for 500 is often isOperational=false if it's an unhandled exception.
+      // Since this class is used to explicitly throw a 500, let's keep it true (default) or make it explicit.
+      // Let's stick to true for explicitly thrown errors.
+    );
+  }
+}
+
+class ServiceUnavailableError extends AppError {
+  constructor(message = httpMessages.SERVICE_UNAVAILABLE) {
+    super(
+      message,
+      httpStatus.SERVICE_UNAVAILABLE,
+      errorTypes.SERVICE_UNAVAILABLE
+    );
+  }
+}
+
+module.exports = {
+  AppError,
+  BadRequestError,
+  AuthenticationError,
+  ForbiddenError,
+  NotFoundError,
+  ConflictError,
+  ValidationError,
+  TooManyRequestsError,
+  PayloadTooLargeError,
+  RequestTimeoutError,
+  InternalServerError,
+  ServiceUnavailableError,
+};
