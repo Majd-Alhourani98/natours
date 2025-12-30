d5c5c4ddb202a30c831e095787a86f81d808bdb8
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2025-12-30 23:45:18 +0200

    feat(auth): integrate email verification and OTP generation into signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,15 +2,30 @@ const User = require('../models/user.model');
 const httpStatus = require('../constants/httpStatus');
 const responseStatus = require('../constants/responseStatus');
 const catchAsync = require('../errors/handlers/catchAsyncHandler');
+const { generateToken, generateOTP } = require('../utils/crypto');
 
 const signup = catchAsync(async (req, res) => {
   const { name, email, password, passwordConfirm } = req.body;
+  const { token, hashedToken, tokenExpires } = generateToken();
+  const { otp, hashedOtp, otpExpires } = generateOTP();
 
-  const user = await User.create({ name, email, password, passwordConfirm });
+  const user = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+    isEmailVerified: false,
+    emailVerificationToken: hashedToken,
+    emailVerificationTokenExpire: tokenExpires,
+    emailVerificationOTP: hashedOtp,
+    emailVerificationTokenExpire: otpExpires,
+  });
 
   res.status(httpStatus.CREATED).json({
     status: responseStatus.SUCCESS,
     data: { user },
+    token,
+    otp,
   });
 });
 
