1bdcb890f879b0cc32d63d5911c6e66ddf7b6372
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-02 12:00:00 +0200

    efactor(app): split app and server
    - move Express app initialization to app.js
    - create server.js to start the server and listen on PORT
    - update package.json scripts:
      - start → node server.js
      - dev → nodemon server.js
    - improves separation of concerns between app configuration and server startup
    - simplifies testing and future scalability

--- app.js
+++ app.js
@@ -7,7 +7,6 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tour.routes');
 const userRouter = require('./routes/user.routes');
 
-// 2. Create an Express app
 const app = express();
 
 app.use(express.json());
@@ -18,9 +17,6 @@ app.use((req, res, next) => {
   req.requestTime = new Date().toISOString();
   next();
 });
-
-// 3. Define the port
-const PORT = 3000;
 
 // health endpoint
 app.get('/health', (req, res) => {
@@ -34,11 +30,4 @@ app.get('/health', (req, res) => {
 app.use('/api/v1/tours', tourRouter);
 app.use('/api/v1/users', userRouter);
 
-// 4. Start the server
-app.listen(PORT, () => {
-  console.log(`\n${'━'.repeat(15)} 🔥 SERVER ${'━'.repeat(15)}`);
-  console.log(`🟢 STATUS      → Running`);
-  console.log(`🔗 LINK        → http://localhost:${PORT}`);
-  console.log(`🌍 ENVIRONMENT → ${app.get('env')}`);
-  console.log(`⏰ STARTED AT  → ${new Date().toLocaleTimeString()}\n`);
-});
+module.exports = app;
---
+++ server.js
@@ -0,0 +1,11 @@
+const app = require('./app');
+
+// 4. Start the server
+const PORT = 3000;
+app.listen(PORT, () => {
+  console.log(`\n${'━'.repeat(15)} 🔥 SERVER ${'━'.repeat(15)}`);
+  console.log(`🟢 STATUS      → Running`);
+  console.log(`🔗 LINK        → http://localhost:${PORT}`);
+  console.log(`🌍 ENVIRONMENT → ${app.get('env')}`);
+  console.log(`⏰ STARTED AT  → ${new Date().toLocaleTimeString()}\n`);
+});
