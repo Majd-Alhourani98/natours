c5fd49247d7a5b60203f7a5038b650a53d10205a
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-02 20:28:04 +0200

    feat(auth): implement resend verification controller

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -2,7 +2,7 @@ const User = require('../models/user.model');
 const httpStatus = require('../constants/httpStatus');
 const responseStatus = require('../constants/responseStatus');
 const catchAsync = require('../errors/handlers/catchAsyncHandler');
-const { ValidationError, ConflictError } = require('../errors/classes/customClasses');
+const { ValidationError, ConflictError, NotFoundError } = require('../errors/classes/customClasses');
 const { sendVerificationEmail } = require('../services/email.service');
 
 const signup = catchAsync(async (req, res, next) => {
@@ -33,7 +33,7 @@ const signup = catchAsync(async (req, res, next) => {
     await user.save({ validateBeforeSave: false });
   }
 
-  res.status(httpStatus.CREATED).json({
+  return res.status(httpStatus.CREATED).json({
     status: responseStatus.SUCCESS,
     message:
       verifyMethod === 'otp'
@@ -44,4 +44,46 @@ const signup = catchAsync(async (req, res, next) => {
   });
 });
 
-module.exports = { signup };
+const resendVerification = catchAsync(async (req, res, next) => {
+  const { email, verifyMethod = 'otp' } = req.body;
+
+  const user = await User.findOne({ email });
+  if (!user) return next(new NotFoundError('User not found with this email'));
+
+  if (user.isEmailVerified)
+    return res.status(httpStatus.OK).json({
+      status: responseStatus.SUCCESS,
+      message: 'Email is already verified.',
+    });
+
+  user.rollbackEmailVerification();
+  await user.save({ validateBeforeSave: false });
+
+  let otp, token;
+  if (verifyMethod === 'otp') {
+    otp = user.createEmailVerificationOTP();
+  } else if (verifyMethod === 'link') {
+    token = user.createEmailVerificationToken();
+  }
+
+  await user.save({ validateBeforeSave: false });
+
+  try {
+    await sendVerificationEmail({ email: user.email, verifyMethod, otp, token });
+  } catch (error) {
+    user.rollbackEmailVerification();
+    await user.save({ validateBeforeSave: false });
+  }
+
+  return res.status(httpStatus.CREATED).json({
+    status: responseStatus.SUCCESS,
+    message:
+      verifyMethod === 'otp'
+        ? 'Signup successful! An OTP has been sent to your email.'
+        : 'Signup successful! A verification link has been sent to your email.',
+
+    data: { user },
+  });
+});
+
+module.exports = { signup, resendVerification };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -4,5 +4,6 @@ const authController = require('../controllers/auth.controller');
 const router = express.Router();
 
 router.route('/signup').post(authController.signup);
+router.route('/resend').post(authController.resendVerification);
 
 module.exports = router;
