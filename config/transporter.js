543c88e812d655c584c1fc44f95c8a1fccbff36d
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-02 17:03:14 +0200

    feat(mail): setup nodemailer transporter with env configuration

---
+++ config/transporter.js
@@ -0,0 +1,18 @@
+const nodemailer = require('nodemailer');
+
+const transporter = nodemailer.createTransport({
+  host: process.env.MAIL_HOST,
+  prot: process.env.MAIL_PORT,
+
+  auth: {
+    user: process.env.MAIL_USER,
+    pass: process.env.MAIL_PASS,
+  },
+});
+
+transporter.verify((err, success) => {
+  if (err) console.error('💥Transporter error:', err);
+  else console.log('Transporter is ready:', success);
+});
+
+module.exports = transporter;
