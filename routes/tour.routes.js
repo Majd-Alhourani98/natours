f86eb3a288f7b66dc7c530d5847903af027aadae
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-04 12:00:00 +0200

    feat: refactor routing to use express.Router for modularity

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const tourController = require('../controllers/tour.controller');
 
-const router = express();
+const router = express.Router();
 
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -2,7 +2,7 @@ const express = require('express');
 
 const userController = require('../controllers/user.controller');
 
-const router = express();
+const router = express.Router();
 
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
