305f4a83c3e5049612431ec4e29c2c44a2c99f08
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-04 20:40:45 +0200

    feat: establish MongoDB connection via Mongoose

--- server.js
+++ server.js
@@ -1,4 +1,21 @@
+const mongoose = require("mongoose");
+
 const app = require("./app");
+
+const DB_URI = "mongodb://localhost:27017/natours";
+mongoose
+  .connect(DB_URI)
+  .then((conn) => {
+    console.log(`\n${"━".repeat(20)} 💾 DATABASE ${"━".repeat(20)}`);
+    console.log(`🟢 STATUS      → Connected`);
+    console.log(`📦 Database:   → ${mongoose.connection.name}`);
+    console.log(`🌐 Host        → ${mongoose.connection.host}`);
+  })
+
+  .catch((error) => {
+    console.error("❌ MongoDB connection failed:", error.message);
+    process.exit(1);
+  });
 
 const PORT = 3000;
 app.listen(PORT, () => {
