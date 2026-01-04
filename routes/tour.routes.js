bd063d780661188cc59b7e747c23f7ce49de1545
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-04 12:00:00 +0200

    add router.param('id', ...) to tourRouter and userRoute

--- routes/tour.routes.js
+++ routes/tour.routes.js
@@ -4,6 +4,12 @@ const tourController = require('../controllers/tour.controller');
 
 const router = express.Router();
 
+router.param('id', (req, res, next, value) => {
+  console.log(value);
+
+  next();
+});
+
 router.route('/').get(tourController.getAllTours).post(tourController.createTour);
 router.route('/:id').get(tourController.getTour).patch(tourController.updateTour).delete(tourController.deleteTour);
 
--- routes/user.routes.js
+++ routes/user.routes.js
@@ -4,6 +4,12 @@ const userController = require('../controllers/user.controller');
 
 const router = express.Router();
 
+router.param('id', (req, res, next, value) => {
+  console.log(value);
+
+  next();
+});
+
 router.route('/').get(userController.getAllUsers).post(userController.createUser);
 router.route('/:id').get(userController.getUser).patch(userController.updateUser).delete(userController.deleteUser);
 
