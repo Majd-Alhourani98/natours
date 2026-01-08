476f63cdc611e3f11b4cc395c96c33859673d76d
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-08 12:00:00 +0200

    feat(tourModel): add pre-aggregate middleware to exclude secret tours

--- models/tour.model.js
+++ models/tour.model.js
@@ -106,6 +106,10 @@ tourSchema.pre(/^find/, function (next) {
   this.find({ secretTour: { $ne: true } });
 });
 
+tourSchema.pre('aggregate', function (next) {
+  this.pipeline().unshift({ $match: { secretTour: { $ne: true } } });
+});
+
 const Tour = mongoose.model('Tour', tourSchema);
 
 module.exports = Tour;
