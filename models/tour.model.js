3e2d198fd3d720e4a7c85efc96a5a92c2f345919
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-10 10:51:48 +0200

    Enhance Tour schema with string and number validation constraints

--- models/tour.model.js
+++ models/tour.model.js
@@ -10,6 +10,8 @@ const tourSchema = new mongoose.Schema(
       required: [true, 'A tour must have a name'],
       unique: true,
       trim: true,
+      maxlength: [40, 'A tour name must have less or equal than 40 characters'],
+      minlength: [10, 'A tour name must have more or equal than 10 characters'],
     },
 
     slug: String,
@@ -27,11 +29,17 @@ const tourSchema = new mongoose.Schema(
     difficulty: {
       type: String,
       required: [true, 'A tour must have a difficulty'],
+      enum: {
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult',
+      },
     },
 
     ratingsAverage: {
       type: Number,
       default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
     },
 
     ratingsQuantity: {
@@ -48,7 +56,7 @@ const tourSchema = new mongoose.Schema(
 
     summary: {
       type: String,
-      trim: true, // only works for strings
+      trim: true,
       required: [true, 'A tour must have a summary'],
     },
 
@@ -63,10 +71,6 @@ const tourSchema = new mongoose.Schema(
     },
 
     images: [String],
-    // createdAt: {
-    //   type: Date,
-    //   default: Date.now(), // give timestamp ==> converted into today's date in mongoose
-    // },
 
     startDates: [Date],
 
@@ -75,6 +79,7 @@ const tourSchema = new mongoose.Schema(
       default: false,
     },
   },
+
   {
     timestamps: true,
     toJSON: { virtuals: true },
