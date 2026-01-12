df3551fc257e3215f0ec30179160df7d57cef06d
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-12 12:00:00 +0200

    feat: handle unhandled promise rejections globally

--- server.js
+++ server.js
@@ -8,10 +8,19 @@ connectDB();
 
 // 4. Start the server
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => {
+const server = app.listen(PORT, () => {
   console.log(`\n${'━'.repeat(15)} 🔥 SERVER ${'━'.repeat(15)}`);
   console.log(`🟢 STATUS      → Running`);
   console.log(`🔗 LINK        → http://localhost:${PORT}`);
   console.log(`🌍 ENVIRONMENT → ${process.env.NODE_ENV}`);
   console.log(`⏰ STARTED AT  → ${new Date().toLocaleTimeString()}\n`);
+});
+
+process.on('unhandledRejection', err => {
+  console.log('UNHANDLED REJECTION! 💥 Shutting down...');
+  console.log(err.name, err.message);
+
+  server.close(() => {
+    process.exit(1);
+  });
 });
