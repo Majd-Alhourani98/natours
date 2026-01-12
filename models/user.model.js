1380ab56c7b8a125431352887be78352def33a16
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-12 09:24:24 +0200

    feat: implement password hashing middleware with bcrypt

--- models/user.model.js
+++ models/user.model.js
@@ -1,4 +1,5 @@
 const mongoose = require('mongoose');
+const bcrypt = require('bcryptjs');
 
 const userSchema = new mongoose.Schema(
   {
@@ -42,6 +43,15 @@ const userSchema = new mongoose.Schema(
     timestamps: true, // <-- automatically adds `createdAt` and `updatedAt` fields
   }
 );
+
+// Pre-save middleware to hash password
+userSchema.pre('save', async function () {
+  if (!this.isModified('password')) return;
+  if (!this.password) return;
+
+  this.password = await bcrypt.hash(this.password, 12);
+  this.passwordConfirm = undefined;
+});
 
 const User = mongoose.model('User', userSchema);
 
