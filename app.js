e3c703a1c09bd6d349307e13c159a896b468f4e9
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-13 15:46:46 +0200

    refactor: move global error handler to dedicated controlle

--- app.js
+++ app.js
@@ -4,6 +4,7 @@ const morgan = require('morgan');
 const tourRouter = require('./routes/tour.routes');
 const userRouter = require('./routes/user.routes');
 const authRouter = require('./routes/auth.routes');
+const globalErrorHandler = require('./errors/globalErrorHandler');
 
 const app = express();
 
@@ -35,14 +36,6 @@ app.all('*', (req, res, next) => {
   next(err);
 });
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || 'error';
-
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
