62d1fb2679d503649be0f6b78d2eacd0ea245cb9
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-13 16:45:32 +0200

    refactor(users): remove try/catch and handle async errors via catchAsync in signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,20 +1,15 @@
+const catchAsync = require('../errors/catchAsync');
 const User = require('../models/user.model');
 
-const signup = async (req, res) => {
+const signup = catchAsync(async (req, res) => {
   const { name, email, password, passwordConfirm } = req.body;
-  try {
-    const user = await User.create({ name, email, password, passwordConfirm });
 
-    res.status(201).json({
-      status: 'success',
-      data: { user: user },
-    });
-  } catch (error) {
-    res.status(400).json({
-      status: 'fail',
-      message: error,
-    });
-  }
-};
+  const user = await User.create({ name, email, password, passwordConfirm });
+
+  res.status(201).json({
+    status: 'success',
+    data: { user: user },
+  });
+});
 
 module.exports = { signup };
