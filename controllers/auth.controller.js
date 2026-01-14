9ffa8857b97d4cb7fe16de95754cd55a6ff4b7f0
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-14 20:43:10 +0200

    refactor: implement sanitizeUser utility for response handling

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,21 +3,18 @@ const responseStatus = require('../constants/responseStatus');
 
 const catchAsync = require('../errors/catchAsync');
 const User = require('../models/user.model');
+const sanitizeUser = require('../utils/sanitizeUser');
 
 const signup = catchAsync(async (req, res) => {
   const { name, email, password, passwordConfirm } = req.body;
 
   const user = await User.create({ name, email, password, passwordConfirm });
 
-  user.password = undefined;
-  user.emailVerificationToken = undefined;
-  user.emailVerificationTokenExpires = undefined;
-  emailVerificationOTP = undefined;
-  emailVerificationOTPExpires = undefined;
+  const cleanUser = sanitizeUser(user);
 
   res.status(httpStatus.CREATED).json({
     status: responseStatus.SUCCESS,
-    data: { user: user },
+    data: { user: cleanUser },
   });
 });
 
