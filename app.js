06691994524c018c5edb3522e46e8c0b31dd0bcc
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-14 19:22:00 +0200

    refactor: move unhandled route handler to separate middleware

--- app.js
+++ app.js
@@ -7,6 +7,7 @@ const tourRouter = require("./routes/tour.routes");
 const userRouter = require("./routes/user.routes");
 const AppError = require("./errors/AppError");
 const notFound = require("./middlewares/notFound");
+const globalErrorHandler = require("./middlewares/globalErrorHandler");
 
 const app = express();
 
@@ -30,14 +31,6 @@ app.use("/api/v1/users", userRouter);
 
 app.all("*", notFound);
 
-app.use((err, req, res, next) => {
-  err.statusCode = err.statusCode || 500;
-  err.status = err.status || "error";
-
-  return res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-  });
-});
+app.use(globalErrorHandler);
 
 module.exports = app;
