34eb7980ea1b9b73055fd71180c0264af17d1d6a
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-14 21:03:57 +0200

    refactor: extract username generation to standalone helper function

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -5,17 +5,29 @@ const catchAsync = require('../errors/catchAsync');
 const User = require('../models/user.model');
 const nanoidLetters = require('../utils/nanoidLetters');
 
-const signup = catchAsync(async (req, res) => {
-  const { name, email, password, passwordConfirm } = req.body;
+const generateUniqueUsername = async (name) => {
   const base = name.replace(/\s+/g, '-').toLowerCase();
   let username = base;
 
   while (await User.findOne({ username }).lean()) {
-    console.log(nanoidLetters);
     username = `${base}_${nanoidLetters()}`.toLowerCase();
   }
 
-  const user = await User.create({ name, email, password, passwordConfirm, username });
+  return username;
+};
+
+const signup = catchAsync(async (req, res) => {
+  const { name, email, password, passwordConfirm } = req.body;
+
+  const username = await generateUniqueUsername(name);
+
+  const user = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+    username,
+  });
 
   res.status(httpStatus.CREATED).json({
     status: responseStatus.SUCCESS,
