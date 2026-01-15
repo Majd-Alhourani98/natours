c032b2c3d3b970f539060311723648fcc73ef3d5
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-16 01:34:03 +0200

    refactor(auth): optimize signup flow by consolidating database writes

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,17 +3,16 @@ const responseStatus = require('../constants/responseStatus');
 
 const catchAsync = require('../errors/catchAsync');
 const User = require('../models/user.model');
-const { genereateToken, generateOtp } = require('../utils/crypto');
 
 const signup = catchAsync(async (req, res) => {
   const { name, email, password, passwordConfirm } = req.body;
 
-  const user = await User.create({ name, email, password, passwordConfirm });
+  const user = new User({ name, email, password, passwordConfirm });
 
   const token = user.generateEmailVerificationToken();
   const otp = user.generateEmailVerificationOtp();
 
-  await user.save({ validateBeforeSave: false });
+  await user.save();
 
   res.status(httpStatus.CREATED).json({
     status: responseStatus.SUCCESS,
