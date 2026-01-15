56bddb671a37f1b9bd4ff226537f2ff01938971c
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-15 18:52:12 -0300

    feat(auth): add email verification token and OTP generation to signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -3,15 +3,29 @@ const responseStatus = require('../constants/responseStatus');
 
 const catchAsync = require('../errors/catchAsync');
 const User = require('../models/user.model');
+const { genereateToken, generateOtp } = require('../utils/crypto');
 
 const signup = catchAsync(async (req, res) => {
   const { name, email, password, passwordConfirm } = req.body;
+  const { token, hashedToken, expiryToken } = genereateToken();
+  const { otp, hashedOtp, expiryOtp } = generateOtp();
 
-  const user = await User.create({ name, email, password, passwordConfirm });
+  const user = await User.create({
+    name,
+    email,
+    password,
+    passwordConfirm,
+    emailVerificationToken: hashedToken,
+    emailVerificationTokenExpires: expiryToken,
+    emailVerificationOTP: hashedOtp,
+    emailVerificationOTPExpires: expiryOtp,
+  });
 
   res.status(httpStatus.CREATED).json({
     status: responseStatus.SUCCESS,
     data: { user },
+    otp,
+    token,
   });
 });
 
--- utils/crypto.js
+++ utils/crypto.js
@@ -6,16 +6,17 @@ const genereateToken = (length = 32, expiryDurationsMs = 10 * 60 * 1000) => {
   const expiryToken = Date.now() + expiryDurationsMs;
 
   return { token, hashedToken, expiryToken };
-
-  //   const minutesRemaining = Math.round((expiryToken - Date.now()) / (1000 * 60));
-  //   console.log(`Minutes until expiry: ${minutesRemaining}`);
 };
 
 const generateOtp = (length = 6, expiryDurationsMs = 10 * 60 * 1000) => {
-  let otp = '';
-  for (let i = 0; i < length; i++) {
-    otp += crypto.randomInt(0, 10);
-  }
+  // let otp = '';
+  // for (let i = 0; i < length; i++) {
+  //   otp += crypto.randomInt(0, 10);
+  // }
+
+  // Generates a random number (e.g., 0 to 999999) and pads with leading zeros
+  const max = Math.pow(10, length);
+  const otp = crypto.randomInt(0, max).toString().padStart(length, '0');
 
   const hashedOtp = crypto.createHash('sha256').update(otp).digest('hex');
   const expiryOtp = Date.now() + expiryDurationsMs;
@@ -27,3 +28,6 @@ console.log(genereateToken());
 console.log(generateOtp());
 
 module.exports = { generateOtp, genereateToken };
+
+//   const minutesRemaining = Math.round((expiryToken - Date.now()) / (1000 * 60));
+//   console.log(`Minutes until expiry: ${minutesRemaining}`);
