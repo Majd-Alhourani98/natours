82ae82fbcc6373fdb93c0b3c554956835923daa2
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-16 14:06:34 +0200

    refactor: allow frontend to choose email verification method

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -4,36 +4,33 @@ const responseStatus = require('../constants/responseStatus');
 const catchAsync = require('../errors/catchAsync');
 const User = require('../models/user.model');
 
-const signupWithToken = catchAsync(async (req, res) => {
+const signup = catchAsync(async (req, res) => {
   const { name, email, password, passwordConfirm } = req.body;
 
   const user = new User({ name, email, password, passwordConfirm });
 
-  const token = user.generateEmailVerificationToken();
+  const verifyMethod = 'otp';
+  let token, otp;
+
+  if (verifyMethod === 'otp') {
+    otp = user.generateEmailVerificationOtp();
+  } else if (verifyMethod === 'link') {
+    token = user.generateEmailVerificationToken();
+  }
 
   await user.save();
 
   res.status(httpStatus.CREATED).json({
     status: responseStatus.SUCCESS,
+    message:
+      verifyMethod === 'otp'
+        ? 'Signup successful! An OTP has been sent to your email.'
+        : 'Signup successful! A verification link has been sent to your email.',
+
     data: { user },
+    otp,
     token,
   });
 });
 
-const signupWithOtp = catchAsync(async (req, res) => {
-  const { name, email, password, passwordConfirm } = req.body;
-
-  const user = new User({ name, email, password, passwordConfirm });
-
-  const otp = user.generateEmailVerificationOtp();
-
-  await user.save();
-
-  res.status(httpStatus.CREATED).json({
-    status: responseStatus.SUCCESS,
-    data: { user },
-    otp,
-  });
-});
-
-module.exports = { signupWithOtp, signupWithToken };
+module.exports = { signup };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -3,7 +3,6 @@ const authController = require('../controllers/auth.controller');
 
 const router = express.Router();
 
-router.route('/signup-with-otp').post(authController.signupWithOtp);
-router.route('/signup-with-token').post(authController.signupWithToken);
+router.route('/signup').post(authController.signup);
 
 module.exports = router;
