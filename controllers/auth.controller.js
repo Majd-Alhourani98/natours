c6e0eef4a438919c74897421d2c43c8937de4d43
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-17 19:52:51 +0200

    feat(utils): implement user data sanitization and update signup flow

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,18 +1,15 @@
 const User = require("../models/user.model");
 const catchAsync = require("../utils/catchAsync");
+const sanitizeUser = require("../utils/sanitizeUser");
 const sendResponse = require("../utils/sendResponse");
 
 const signup = catchAsync(async (req, res, next) => {
   const { email, name, password, passwordConfirm } = req.body;
   const user = await User.create({ email, name, password, passwordConfirm });
 
-  user.password = undefined;
-  user.emailVerificationToken = undefined;
-  user.emailVerificationTokenExpires = undefined;
-  emailVerificationOTP = undefined;
-  emailVerificationOTPExpires = undefined;
+  const cleanedUser = sanitizeUser(user);
 
-  sendResponse(res, { data: { user } });
+  sendResponse(res, { data: { user: cleanedUser } });
 });
 
 module.exports = { signup };
---
+++ utils/sanitizeUser.js
@@ -0,0 +1,14 @@
+const sanitizeUser = (user) => {
+  const sanitized = { ...user._doc };
+  delete sanitized.password;
+  delete sanitized.emailVerificationToken;
+  delete sanitized.emailVerificationTokenExpires;
+  delete sanitized.passwordResetToken;
+  delete sanitized.passwordResetExpires;
+  delete sanitized.emailVerificationOTP;
+  delete sanitized.emailVerificationOTPExpires;
+
+  return sanitized;
+};
+
+module.exports = sanitizeUser;
