6c43413b23a534a1fb1b59b7c9e6b3b44340ee18
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-17 19:27:45 +0200

    feat: implement User model with Mongoose schema and validation

---
+++ models/user.model.js
@@ -0,0 +1,43 @@
+const mongoose = require("mongoose");
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, "Please tell us your name"],
+      trim: true,
+      minlength: [3, "Name must be at least 3 characters"],
+      maxlength: [50, "Name must be less than 50 characters"],
+    },
+
+    email: {
+      type: String,
+      required: [true, "Please provide your email"],
+      unique: true,
+      lowercase: true,
+      trim: true,
+      match: [
+        /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/,
+        "Please provide a valid email",
+      ],
+    },
+
+    password: {
+      type: String,
+      required: [true, "Please provide a password"],
+      minlength: [8, "Password must be at least 8 characters"],
+    },
+
+    passwordConfirm: {
+      type: String,
+      required: [true, "Please confirm your password"],
+    },
+  },
+
+  {
+    timestamps: true, // <-- automatically adds `createdAt` and `updatedAt` fields
+  },
+);
+
+const User = mongoose.model("User", userSchema);
+module.exports = User;
