fc2ee23d26e63b793eef08ac6af8828873fd4f63
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-18 21:59:44 +0200

    - Created crypto utility to generate secure tokens and OTPs using SHA-256 hashing.

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -1,9 +1,13 @@
 const User = require("../models/user.model");
 const catchAsync = require("../utils/catchAsync");
+const { generateToken, generateOtp } = require("../utils/crypto");
 const sendResponse = require("../utils/sendResponse");
 
 const signup = catchAsync(async (req, res, next) => {
   const { email, name, password, passwordConfirm } = req.body;
+
+  const { token, hashedToken, tokenExpires } = generateToken();
+  const { otp, hashedOtp, otpExpires } = generateOtp();
 
   const user = await User.create({
     email,
@@ -11,9 +15,13 @@ const signup = catchAsync(async (req, res, next) => {
     password,
     passwordConfirm,
     isEmailVerified: false,
+    emailVerificationToken: hashedToken,
+    emailVerificationTokenExpires: tokenExpires,
+    emailVerificationOTP: hashedOtp,
+    emailVerificationOTPExpires: otpExpires,
   });
 
-  sendResponse(res, { statusCode: 201, data: { user: user } });
+  sendResponse(res, { statusCode: 201, data: { user: user, otp, token } });
 });
 
 module.exports = { signup };
---
+++ utils/crypto.js
@@ -0,0 +1,24 @@
+const crypto = require("crypto");
+
+const generateToken = (length = 32, expiryDurationMs = 10 * 60 * 1000) => {
+  const token = crypto.randomBytes(length).toString("hex");
+  const hashedToken = crypto.createHash("sha256").update(token).digest("hex");
+  const tokenExpires = Date.now() + expiryDurationMs;
+
+  return { token, hashedToken, tokenExpires };
+};
+
+const generateOtp = (length = 6, expiryDurationMs = 10 * 60 * 1000) => {
+  // Generates a random numeric string of specific length
+  const otp = crypto
+    .randomInt(0, Math.pow(10, length))
+    .toString()
+    .padStart(length, "0");
+
+  const hashedOtp = crypto.createHash("sha256").update(otp).digest("hex");
+  const otpExpires = Date.now() + expiryDurationMs;
+
+  return { otp, hashedOtp, otpExpires };
+};
+
+module.exports = { generateToken, generateOtp };
