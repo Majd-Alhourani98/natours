246d0994c5393ca8553bc0e9220d4af0287669d8
Majd Aldein Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-18 12:00:00 +0200

    refactor(errors): move sendErrorDev and sendErrorProd to errors/sendErrorResponse.js

--- errors/globalError.js
+++ errors/globalError.js
@@ -1,42 +1,5 @@
-const { generateErrorId } = require('../utils/nanoid');
+const { sendErrorProd, sendErrorDev } = require('./sendErrorResponse');
 const transformError = require('./transformError');
-
-const sendErrorDev = (err, res) => {
-  res.status(err.statusCode).json({
-    status: err.status,
-    message: err.message,
-    stack: err.stack,
-    error: err,
-  });
-};
-
-const sendErrorProd = (err, req, res) => {
-  if (err.isOperational) {
-    return res.status(err.statusCode).json({
-      status: err.status,
-      message: err.message,
-    });
-  } else {
-    console.log(err);
-    const errorId = generateErrorId();
-    console.error('💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥 CRITICAL ERROR 💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥');
-    console.error(`ID:        ${errorId}`);
-    console.error(`Time:      ${new Date().toISOString()}`);
-    console.error(`Path:      ${req.method} ${req.originalUrl}`);
-    console.error(`IP:        ${req.ip}`);
-    console.error(`Message:   ${err.message}`);
-    console.error(`Stack:     ${err.stack}`);
-    console.error(`Error:     ${err}`);
-    console.error('💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥💥');
-
-    // 2) Send professional, clean message to client
-    return res.status(500).json({
-      status: 'error',
-      message: 'An unexpected error occurred. Please try again later.',
-      supportId: errorId, // Sending it as a separate field is cleaner for frontend handling
-    });
-  }
-};
 
 const globalError = (err, req, res, next) => {
   err.statusCode = err.statusCode || 500;
