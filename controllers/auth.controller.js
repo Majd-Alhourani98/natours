5eedbfc6d4bf6ad2a8b5ef28d26fcc56a288a58e
Majd Alhourani <majd.aldein.alhourani@gmail.com>
2026-01-18 22:26:48 +0200

    refactor(auth): let backend decide email verification method during signup

--- controllers/auth.controller.js
+++ controllers/auth.controller.js
@@ -13,57 +13,19 @@ const signup = catchAsync(async (req, res, next) => {
     isEmailVerified: false,
   });
 
-  const token = user.generateEmailVerificationToken();
-  const otp = user.generateEmailVerificationOtp();
+  let token,
+    otp,
+    verifyMethod = "otp";
+
+  if (verifyMethod === "otp") {
+    otp = user.generateEmailVerificationOtp();
+  } else if (verifyMethod == "link") {
+    token = user.generateEmailVerificationToken();
+  }
 
   await user.save();
 
   sendResponse(res, { statusCode: 201, data: { user: user, otp, token } });
 });
 
-const signupWithOtp = catchAsync(async (req, res, next) => {
-  const { email, name, password, passwordConfirm } = req.body;
-
-  const user = new User({
-    email,
-    name,
-    password,
-    passwordConfirm,
-    isEmailVerified: false,
-  });
-
-  const otp = user.generateEmailVerificationOtp();
-
-  await user.save();
-
-  sendResponse(res, {
-    statusCode: 201,
-    message:
-      "Signup successful! Please verify using the OTP sent to your email.",
-    data: { user: user, otp },
-  });
-});
-
-const signupWithToken = catchAsync(async (req, res, next) => {
-  const { email, name, password, passwordConfirm } = req.body;
-
-  const user = new User({
-    email,
-    name,
-    password,
-    passwordConfirm,
-    isEmailVerified: false,
-  });
-
-  const token = user.generateEmailVerificationToken();
-
-  await user.save();
-
-  sendResponse(res, {
-    statusCode: 201,
-    message: "Signup successful! Please verify your email via the link.",
-    data: { user: user, token },
-  });
-});
-
-module.exports = { signup, signupWithOtp, signupWithToken };
+module.exports = { signup };
--- routes/auth.routes.js
+++ routes/auth.routes.js
@@ -5,7 +5,5 @@ const authController = require("../controllers/auth.controller");
 const router = express.Router();
 
 router.route("/signup").post(authController.signup);
-router.route("/signup-with-token").post(authController.signupWithToken);
-router.route("/signup-with-otp").post(authController.signupWithOtp);
 
 module.exports = router;
